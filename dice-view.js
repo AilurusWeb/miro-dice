78118ddbbedfa3b43d7d66c14ae15aaa08ba3342
Jeanmarie Huchet <contact@jeanmarie-huchet.fr>
2020-04-08 15:31:34 +0200

    Begin system roll dice in tchat

--- dice-view.js
+++ dice-view.js
@@ -6,10 +6,55 @@ let repliesContain = document.getElementById('repliesContain')
 
 /* Functions */
 
+// Check Response contains rolls
+// return array || false
+function hasRollsString(str) {
+  if(typeof str !== 'string') return false
+  let words = str.split(" ")
+  // check that this string does not have other unwanted characters
+  // d D chiffre + -
+  const hasBadChars = /[^d^\d^\+^\-]/gi;
+  // 
+  // 
+  const onceD = /d{2,}/gi;
+  // check that this string has good format
+  // 5d5 5D5 5d5+5 5d5+5-5
+  const rollFormat = /(\d*?D\d+)(((\+|-)\d+)*)?/gi;
+
+  for (let word of words) {
+    console.log(word, "Begin")
+    if (word === null) continue
+    if (word.search(hasBadChars) !== -1) continue
+    if (word.match(onceD) !== null) continue
+    if (word.match(rollFormat) === null) continue
+
+    console.log(word, ", it's perfect !")
+  }
+//  let results = str.match(regex) || []
+//  console.log(results, " --")
+//  if (results.length > 0) return results.map(str => str.trim())
+//  else  return false
+}
+
+// Convert string roll to calculate 
+// 5d6+3 6d6+2 5d6
+function calcRolls(rolls) {
+  if(!Array.isArray(rolls) || rolls.length < 1) return false
+
+//  console.log(rolls, "rolls")
+  let results = []
+  for ( const roll of rolls ) {
+    results.push({
+      dice: 1,
+      bonus: 2
+    }) 
+
+  }
+  //console.log()
+}
+
 function inputValidation() {
-  const regex = "";
   if (inputRoll.value.length <= 0) return false
-  if (false) return false
   return true
 }
 
@@ -17,12 +62,15 @@ function resultRoll() {
   console.log("Roulez !")
 }
 
-function reply(username, result) {
+function reply(username, response) {
   let reply = document.createElement("div");
       reply.className = "rolldice__reply";
       reply.innerHTML = `<span class="rolldice__reply-username">${username}</span>
-                         <span class="rolldice__reply-result">${result}</span>`;
+                         <span class="rolldice__reply-result">${response}</span>`;
   repliesContain.appendChild(reply)
+  let rolls = hasRollsString(response)
+  calcRolls(rolls)
+  inputRoll.value = ''
 }
 
 /* Events */
